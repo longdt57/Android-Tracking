34527e1f93c4126ab9219efddb0eef23e4bbf2e4
Long Do <Longdt57@gmail.com>
2025-02-28 21:43:07 +0900

    Add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,40 @@ plugins {
     alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.google.service) apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.0"
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+detekt {
+    toolVersion = "1.23.0"
+
+    source = files(
+        "app/src/main/java",
+        "analytics/src/main/java"
+    )
+    parallel = false
+    config = files("detekt-config.yml")
+    buildUponDefaultConfig = false
+    disableDefaultRuleSets = false
+
+    debug = false
+    ignoreFailures = false
+
+    ignoredBuildTypes = listOf(libs.versions.release.get())
+    ignoredFlavors = listOf(libs.versions.prod.get())
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    jvmTarget = JavaVersion.VERSION_17.toString()
+    reports {
+        xml {
+            outputLocation.set(file("build/reports/detekt/detekt.xml"))
+        }
+        html {
+            outputLocation.set(file("build/reports/detekt/detekt.html"))
+        }
+    }
 }
